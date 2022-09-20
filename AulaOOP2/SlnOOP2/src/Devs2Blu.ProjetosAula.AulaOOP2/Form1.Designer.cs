8d2b344f25a4a93023b1bf7f3d130416e2feb68f
Eduardo Xavier <eduardoxavi15@gmail.com>
2022-09-19 22:10:07 -0300

    oop 2 evolution

---
+++ AulaOOP2/SlnOOP2/src/Devs2Blu.ProjetosAula.AulaOOP2/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Devs2Blu.ProjetosAula.AulaOOP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblTelCel = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCEP = new System.Windows.Forms.Label();
+            this.lblRua = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTelCel = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(13, 13);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(58, 18);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblTelCel
+            // 
+            this.lblTelCel.AutoSize = true;
+            this.lblTelCel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelCel.Location = new System.Drawing.Point(13, 48);
+            this.lblTelCel.Name = "lblTelCel";
+            this.lblTelCel.Size = new System.Drawing.Size(66, 18);
+            this.lblTelCel.TabIndex = 1;
+            this.lblTelCel.Text = "Tel/Cel:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(13, 80);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(61, 18);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCEP.Location = new System.Drawing.Point(13, 116);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(47, 18);
+            this.lblCEP.TabIndex = 3;
+            this.lblCEP.Text = "CEP:";
+            // 
+            // lblRua
+            // 
+            this.lblRua.AutoSize = true;
+            this.lblRua.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRua.Location = new System.Drawing.Point(13, 150);
+            this.lblRua.Name = "lblRua";
+            this.lblRua.Size = new System.Drawing.Size(43, 18);
+            this.lblRua.TabIndex = 4;
+            this.lblRua.Text = "Rua:";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBairro.Location = new System.Drawing.Point(13, 215);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(59, 18);
+            this.lblBairro.TabIndex = 5;
+            this.lblBairro.Text = "Bairro:";
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCidade.Location = new System.Drawing.Point(13, 249);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(65, 18);
+            this.lblCidade.TabIndex = 6;
+            this.lblCidade.Text = "Cidade:";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(14, 281);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(66, 18);
+            this.lblEstado.TabIndex = 7;
+            this.lblEstado.Text = "Estado:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.SystemColors.Control;
+            this.txtNome.Location = new System.Drawing.Point(85, 11);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(204, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtTelCel
+            // 
+            this.txtTelCel.BackColor = System.Drawing.SystemColors.Control;
+            this.txtTelCel.Location = new System.Drawing.Point(85, 46);
+            this.txtTelCel.Name = "txtTelCel";
+            this.txtTelCel.Size = new System.Drawing.Size(204, 20);
+            this.txtTelCel.TabIndex = 9;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.SystemColors.Control;
+            this.txtEmail.Location = new System.Drawing.Point(85, 78);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(204, 20);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.BackColor = System.Drawing.SystemColors.Control;
+            this.txtCEP.Location = new System.Drawing.Point(85, 114);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(204, 20);
+            this.txtCEP.TabIndex = 11;
+            this.txtCEP.TextChanged += new System.EventHandler(this.txtCEP_TextChanged);
+            // 
+            // txtRua
+            // 
+            this.txtRua.BackColor = System.Drawing.SystemColors.Control;
+            this.txtRua.Location = new System.Drawing.Point(85, 151);
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(204, 20);
+            this.txtRua.TabIndex = 12;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.BackColor = System.Drawing.SystemColors.Control;
+            this.txtBairro.Location = new System.Drawing.Point(85, 213);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(204, 20);
+            this.txtBairro.TabIndex = 13;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.BackColor = System.Drawing.SystemColors.Control;
+            this.txtCidade.Location = new System.Drawing.Point(85, 247);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(204, 20);
+            this.txtCidade.TabIndex = 14;
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.BackColor = System.Drawing.SystemColors.Control;
+            this.txtEstado.Location = new System.Drawing.Point(85, 279);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(204, 20);
+            this.txtEstado.TabIndex = 15;
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.BackColor = System.Drawing.SystemColors.Control;
+            this.txtConsole.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsole.Location = new System.Drawing.Point(331, 42);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
+            this.txtConsole.Size = new System.Drawing.Size(293, 339);
+            this.txtConsole.TabIndex = 16;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(34, 351);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(103, 33);
+            this.btnEnviar.TabIndex = 17;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Yellow;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(186, 351);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(103, 33);
+            this.btnLimpar.TabIndex = 18;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(14, 184);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(73, 18);
+            this.lblNumero.TabIndex = 19;
+            this.lblNumero.Text = "Numero:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.BackColor = System.Drawing.SystemColors.Control;
+            this.txtNumero.Location = new System.Drawing.Point(85, 182);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(204, 20);
+            this.txtNumero.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(449, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Contatos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(664, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtRua);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelCel);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.lblRua);
+            this.Controls.Add(this.lblCEP);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTelCel);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblTelCel;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblCEP;
+        private System.Windows.Forms.Label lblRua;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTelCel;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.TextBox txtRua;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AulaOOP2/SlnOOP2/src/Devs2Blu.ProjetosAula.AulaOOP2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using Devs2Blu.ProjetosAula.OOP2Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Devs2Blu.ProjetosAula.AulaOOP2
+{
+    public partial class Form1 : Form
+    {
+        public Contato Contato { get; set; }
+
+        public void BindData()
+        {
+            Contato.Nome = txtNome.Text;
+            Contato.TelCel = txtTelCel.Text;
+            Contato.Email = txtEmail.Text;
+            Contato.CEP = txtCEP.Text;
+            Contato.Rua = txtRua.Text;
+            Contato.Numero = Int32.Parse(txtNumero.Text);
+            Contato.Bairro = txtBairro.Text;
+            Contato.Cidade = txtCidade.Text;
+            Contato.Estado = txtEstado.Text;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventos
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Contato = new Contato();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (!ValidaForm())
+            {
+                MessageBox.Show(this, "Preencha todos os campos", "Erro - Formulário de contato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            BindData();
+            String textoMsg = $"{txtNome.Text}, enviado com sucesso!";
+            MessageBox.Show(this, textoMsg, "Formulario de contato");
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparForm();
+        }
+
+        private void txtCEP_TextChanged(object sender, EventArgs e)
+        {
+            if(txtCEP.Text.Length == 8)
+            {
+                txtRua.Text = "7 de Setembro";
+                txtBairro.Text = "Centro";
+                txtCidade.Text = "Blumenau";
+                txtEstado.Text = "SC";
+            }
+            else
+            {
+                txtRua.Clear();
+                txtBairro.Clear();
+                txtCidade.Clear();
+                txtEstado.Clear();
+            }
+        }
+
+        #endregion
+
+        #region Metodos
+        public void LimparForm()
+        {
+            txtNome.Clear();
+            txtTelCel.Clear();
+            txtEmail.Clear();
+            txtCEP.Clear();
+            txtRua.Clear();
+            txtNumero.Clear();
+            txtBairro.Clear();
+            txtCidade.Clear();
+            txtEstado.Clear();
+        }
+
+        public bool ValidaForm()
+        {
+            if (txtNome.Text.Equals("")) return false;
+            if (txtTelCel.Text.Equals("")) return false;
+            if (txtEmail.Text.Equals("")) return false;
+            if (txtCEP.Text.Equals("")) return false;
+            if (txtRua.Text.Equals("")) return false;
+            if (txtNumero.Text.Equals("")) return false;
+            if (txtBairro.Text.Equals("")) return false;
+            if (txtCidade.Text.Equals("")) return false;
+            if (txtEstado.Text.Equals("")) return false;
+            return true;
+        }
+
+        #endregion
+
+        
+    }
+}
