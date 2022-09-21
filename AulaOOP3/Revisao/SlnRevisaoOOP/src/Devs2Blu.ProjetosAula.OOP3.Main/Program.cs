b8982dafaa4c22b5b5bddc9b0a74619564606947
Eduardo Xavier <eduardoxavi15@gmail.com>
2022-09-21 15:25:46 -0300

    Aula7 reforco OOP

---
+++ AulaOOP3/Revisao/SlnRevisaoOOP/src/Devs2Blu.ProjetosAula.OOP3.Main/Program.cs
@@ -0,0 +1,36 @@
+﻿using Devs2Blu.ProjetosAula.OOP3.Main.Utils;
+using Devs2Blu.ProjetosAula.OOP3.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.ProjetosAula.OOP3.Main
+{
+    class Program
+    {
+        public static Mocks Mock { get; set; }
+        static void Main(string[] args)
+        {
+            Mock = new Mocks();
+            ViewListPacientes();
+        }
+
+        public static void ViewListPacientes()
+        {
+            Console.Clear();
+
+            foreach (Paciente paciente in Mock.ListaPacientes)
+            {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine($"Nome: {paciente.CodigoPaciente}");
+                Console.WriteLine($"Nome: {paciente.Nome}");
+                Console.WriteLine($"Nome: {paciente.CGCCPF}");
+                Console.WriteLine($"Nome: {paciente.Convenio}");
+                Console.WriteLine($"Nome: {paciente.TipoPessoa}");
+                Console.WriteLine("-----------------------------------\n");
+            }
+        }
+    }
+}
