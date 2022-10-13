eac78d2857f8c3e6ed9e946a04c6af27d2c7f1bb
Eduardo Xavier <eduardoxavi15@gmail.com>
2022-10-13 16:34:54 -0300

    [Agenda contatos] Telas prontas

--- Banco de Dados/ProjetoSistemaAgendaContatos/SlnSistemaAgendaContatos/src/ProjetoDevs2Blu.SistemaAgendaContatos/Form1.Designer.cs
+++ Banco de Dados/ProjetoSistemaAgendaContatos/SlnSistemaAgendaContatos/src/ProjetoDevs2Blu.SistemaAgendaContatos/Form1.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.gpCadastroContato = new System.Windows.Forms.GroupBox();
+            this.cboUF = new System.Windows.Forms.ComboBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.txtRedeSocial = new System.Windows.Forms.TextBox();
+            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.label12 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
@@ -45,23 +53,19 @@
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.gpCadastroCompromisso = new System.Windows.Forms.GroupBox();
+            this.dtpDataCompromisso = new System.Windows.Forms.DateTimePicker();
             this.label13 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.dgvContatos = new System.Windows.Forms.DataGridView();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
-            this.txtRedeSocial = new System.Windows.Forms.TextBox();
-            this.dtpDataCompromisso = new System.Windows.Forms.DateTimePicker();
-            this.txtRua = new System.Windows.Forms.TextBox();
-            this.txtNumero = new System.Windows.Forms.TextBox();
-            this.txtBairro = new System.Windows.Forms.TextBox();
-            this.txtCidade = new System.Windows.Forms.TextBox();
-            this.cboUF = new System.Windows.Forms.ComboBox();
             this.dtvCompromissos = new System.Windows.Forms.DataGridView();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnInsert = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.cboDiaSemana = new System.Windows.Forms.ComboBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
             this.gpCadastroContato.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.gpCadastroCompromisso.SuspendLayout();
@@ -98,9 +102,67 @@
             this.gpCadastroContato.TabStop = false;
             this.gpCadastroContato.Text = "Cadastro de Contato";
             // 
+            // cboUF
+            // 
+            this.cboUF.FormattingEnabled = true;
+            this.cboUF.Location = new System.Drawing.Point(388, 144);
+            this.cboUF.Name = "cboUF";
+            this.cboUF.Size = new System.Drawing.Size(56, 21);
+            this.cboUF.TabIndex = 17;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(61, 143);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(221, 20);
+            this.txtCidade.TabIndex = 16;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(61, 113);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(117, 20);
+            this.txtBairro.TabIndex = 15;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(252, 86);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(30, 20);
+            this.txtNumero.TabIndex = 14;
+            // 
+            // txtRua
+            // 
+            this.txtRua.Location = new System.Drawing.Point(61, 84);
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(154, 20);
+            this.txtRua.TabIndex = 13;
+            // 
+            // txtRedeSocial
+            // 
+            this.txtRedeSocial.Location = new System.Drawing.Point(307, 54);
+            this.txtRedeSocial.Name = "txtRedeSocial";
+            this.txtRedeSocial.Size = new System.Drawing.Size(139, 20);
+            this.txtRedeSocial.TabIndex = 12;
+            // 
+            // mskTelefone
+            // 
+            this.mskTelefone.Location = new System.Drawing.Point(356, 24);
+            this.mskTelefone.Mask = "(00) 00000-0000";
+            this.mskTelefone.Name = "mskTelefone";
+            this.mskTelefone.Size = new System.Drawing.Size(90, 20);
+            this.mskTelefone.TabIndex = 11;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(61, 54);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(154, 20);
+            this.txtEmail.TabIndex = 10;
+            // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(61, 20);
+            this.txtNome.Location = new System.Drawing.Point(61, 25);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(221, 20);
             this.txtNome.TabIndex = 9;
@@ -110,7 +172,7 @@
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.SystemColors.Control;
-            this.label12.Location = new System.Drawing.Point(72, 137);
+            this.label12.Location = new System.Drawing.Point(9, 144);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(51, 18);
             this.label12.TabIndex = 8;
@@ -121,7 +183,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.Control;
-            this.label8.Location = new System.Drawing.Point(24, 105);
+            this.label8.Location = new System.Drawing.Point(10, 113);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(45, 18);
             this.label8.TabIndex = 7;
@@ -132,18 +194,18 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.Control;
-            this.label7.Location = new System.Drawing.Point(241, 72);
+            this.label7.Location = new System.Drawing.Point(225, 88);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(58, 18);
+            this.label7.Size = new System.Drawing.Size(25, 18);
             this.label7.TabIndex = 6;
-            this.label7.Text = "Numero";
+            this.label7.Text = "Nº";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.Control;
-            this.label6.Location = new System.Drawing.Point(24, 72);
+            this.label6.Location = new System.Drawing.Point(23, 85);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(32, 18);
             this.label6.TabIndex = 5;
@@ -154,7 +216,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.Control;
-            this.label5.Location = new System.Drawing.Point(302, 137);
+            this.label5.Location = new System.Drawing.Point(336, 145);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(48, 18);
             this.label5.TabIndex = 4;
@@ -165,7 +227,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.Control;
-            this.label4.Location = new System.Drawing.Point(221, 47);
+            this.label4.Location = new System.Drawing.Point(225, 54);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 18);
             this.label4.TabIndex = 3;
@@ -176,7 +238,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(15, 46);
+            this.label3.Location = new System.Drawing.Point(15, 54);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 18);
             this.label3.TabIndex = 2;
@@ -187,7 +249,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(288, 20);
+            this.label2.Location = new System.Drawing.Point(288, 25);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(62, 18);
             this.label2.TabIndex = 1;
@@ -198,7 +260,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(14, 20);
+            this.label1.Location = new System.Drawing.Point(14, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 18);
             this.label1.TabIndex = 0;
@@ -245,6 +307,9 @@
             // gpCadastroCompromisso
             // 
             this.gpCadastroCompromisso.BackColor = System.Drawing.SystemColors.Highlight;
+            this.gpCadastroCompromisso.Controls.Add(this.txtStatus);
+            this.gpCadastroCompromisso.Controls.Add(this.cboDiaSemana);
+            this.gpCadastroCompromisso.Controls.Add(this.txtTitulo);
             this.gpCadastroCompromisso.Controls.Add(this.dtpDataCompromisso);
             this.gpCadastroCompromisso.Controls.Add(this.label13);
             this.gpCadastroCompromisso.Controls.Add(this.label11);
@@ -257,6 +322,13 @@
             this.gpCadastroCompromisso.TabIndex = 1;
             this.gpCadastroCompromisso.TabStop = false;
             this.gpCadastroCompromisso.Text = "Cadastro de Compromisso";
+            // 
+            // dtpDataCompromisso
+            // 
+            this.dtpDataCompromisso.Location = new System.Drawing.Point(268, 57);
+            this.dtpDataCompromisso.Name = "dtpDataCompromisso";
+            this.dtpDataCompromisso.Size = new System.Drawing.Size(89, 20);
+            this.dtpDataCompromisso.TabIndex = 10;
             // 
             // label13
             // 
@@ -287,9 +359,9 @@
             this.label10.ForeColor = System.Drawing.SystemColors.Control;
             this.label10.Location = new System.Drawing.Point(14, 58);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(106, 18);
+            this.label10.Size = new System.Drawing.Size(100, 18);
             this.label10.TabIndex = 6;
-            this.label10.Text = "Dias da Semana";
+            this.label10.Text = "Dia da Semana";
             // 
             // label9
             // 
@@ -312,71 +384,6 @@
             this.dgvContatos.ReadOnly = true;
             this.dgvContatos.Size = new System.Drawing.Size(456, 209);
             this.dgvContatos.TabIndex = 2;
-            // 
-            // txtEmail
-            // 
-            this.txtEmail.Location = new System.Drawing.Point(61, 46);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(154, 20);
-            this.txtEmail.TabIndex = 10;
-            // 
-            // mskTelefone
-            // 
-            this.mskTelefone.Location = new System.Drawing.Point(356, 19);
-            this.mskTelefone.Mask = "(00) 00000-0000";
-            this.mskTelefone.Name = "mskTelefone";
-            this.mskTelefone.Size = new System.Drawing.Size(90, 20);
-            this.mskTelefone.TabIndex = 11;
-            // 
-            // txtRedeSocial
-            // 
-            this.txtRedeSocial.Location = new System.Drawing.Point(307, 47);
-            this.txtRedeSocial.Name = "txtRedeSocial";
-            this.txtRedeSocial.Size = new System.Drawing.Size(139, 20);
-            this.txtRedeSocial.TabIndex = 12;
-            // 
-            // dtpDataCompromisso
-            // 
-            this.dtpDataCompromisso.Location = new System.Drawing.Point(268, 57);
-            this.dtpDataCompromisso.Name = "dtpDataCompromisso";
-            this.dtpDataCompromisso.Size = new System.Drawing.Size(89, 20);
-            this.dtpDataCompromisso.TabIndex = 10;
-            // 
-            // txtRua
-            // 
-            this.txtRua.Location = new System.Drawing.Point(62, 72);
-            this.txtRua.Name = "txtRua";
-            this.txtRua.Size = new System.Drawing.Size(154, 20);
-            this.txtRua.TabIndex = 13;
-            // 
-            // txtNumero
-            // 
-            this.txtNumero.Location = new System.Drawing.Point(319, 70);
-            this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new System.Drawing.Size(126, 20);
-            this.txtNumero.TabIndex = 14;
-            // 
-            // txtBairro
-            // 
-            this.txtBairro.Location = new System.Drawing.Point(75, 105);
-            this.txtBairro.Name = "txtBairro";
-            this.txtBairro.Size = new System.Drawing.Size(126, 20);
-            this.txtBairro.TabIndex = 15;
-            // 
-            // txtCidade
-            // 
-            this.txtCidade.Location = new System.Drawing.Point(125, 137);
-            this.txtCidade.Name = "txtCidade";
-            this.txtCidade.Size = new System.Drawing.Size(126, 20);
-            this.txtCidade.TabIndex = 16;
-            // 
-            // cboUF
-            // 
-            this.cboUF.FormattingEnabled = true;
-            this.cboUF.Location = new System.Drawing.Point(360, 135);
-            this.cboUF.Name = "cboUF";
-            this.cboUF.Size = new System.Drawing.Size(56, 21);
-            this.cboUF.TabIndex = 17;
             // 
             // dtvCompromissos
             // 
@@ -413,11 +420,46 @@
             this.btnInsert.TabIndex = 3;
             this.btnInsert.UseVisualStyleBackColor = true;
             // 
+            // btnInfo
+            // 
+            this.btnInfo.BackgroundImage = global::ProjetoDevs2Blu.SistemaAgendaContatos.Properties.Resources.Info2;
+            this.btnInfo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnInfo.FlatAppearance.BorderSize = 0;
+            this.btnInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInfo.Location = new System.Drawing.Point(742, 143);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(89, 61);
+            this.btnInfo.TabIndex = 7;
+            this.btnInfo.UseVisualStyleBackColor = true;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(63, 28);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(152, 20);
+            this.txtTitulo.TabIndex = 18;
+            // 
+            // cboDiaSemana
+            // 
+            this.cboDiaSemana.FormattingEnabled = true;
+            this.cboDiaSemana.Location = new System.Drawing.Point(120, 54);
+            this.cboDiaSemana.Name = "cboDiaSemana";
+            this.cboDiaSemana.Size = new System.Drawing.Size(95, 21);
+            this.cboDiaSemana.TabIndex = 18;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(268, 29);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(54, 20);
+            this.txtStatus.TabIndex = 19;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(854, 450);
+            this.Controls.Add(this.btnInfo);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.dtvCompromissos);
             this.Controls.Add(this.btnInsert);
@@ -477,6 +519,10 @@
         private System.Windows.Forms.ComboBox cboUF;
         private System.Windows.Forms.DataGridView dtvCompromissos;
         private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.ComboBox cboDiaSemana;
+        private System.Windows.Forms.TextBox txtTitulo;
     }
 }
 
--- Banco de Dados/ProjetoSistemaAgendaContatos/SlnSistemaAgendaContatos/src/ProjetoDevs2Blu.SistemaAgendaContatos/Properties/Resources.Designer.cs
+++ Banco de Dados/ProjetoSistemaAgendaContatos/SlnSistemaAgendaContatos/src/ProjetoDevs2Blu.SistemaAgendaContatos/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace ProjetoDevs2Blu.SistemaAgendaContatos.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Info2 {
+            get {
+                object obj = ResourceManager.GetObject("Info2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Insert {
             get {
                 object obj = ResourceManager.GetObject("Insert", resourceCulture);
